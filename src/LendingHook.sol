85c89102ec48b81b7f6f27a83833d07e0eafaa07
Ankit Kumar Pal <ankitkumarpal27@gmail.com>
2025-05-27 11:29:00 +0200

    feat: setup test

--- src/LendingHook.sol
+++ src/LendingHook.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.26;
 
 import {IPoolManager} from "v4-periphery/lib/v4-core/src/interfaces/IPoolManager.sol";
 import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
-import {Hooks} from "v4-periphery/lib/v4-core/src/libraries/Hooks.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 import {Currency, CurrencyLibrary} from "v4-periphery/lib/v4-core/src/types/Currency.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {PoolKey} from "v4-periphery/lib/v4-core/src/types/PoolKey.sol";
-import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {Variables} from "./Variables.sol";
 import {Lending} from "./Lending.sol";
 
@@ -65,5 +65,9 @@ contract LendingHook is BaseHook, Variables {
 
     function setLending(address _lender) public onlyOwner {
         lending = Lending(_lender);
+    }
+
+    function getHookData(address user) public pure returns (bytes memory) {
+        return abi.encode(user);
     }
 }
--- test/Lending.t.sol
+++ test/Lending.t.sol
@@ -3,30 +3,77 @@ pragma solidity ^0.8.26;
 
 import "forge-std/Test.sol";
 import {GasSnapshot} from "forge-gas-snapshot/GasSnapshot.sol";
-
-// // test erc20
-// import {TestERC20} from "v4-core/test/TestERC20.sol";
-
-// // libraries
-// import {Currency, CurrencyLibrary} from "v4-periphery/lib/v4-core/src/types/Currency.sol";
-// import {PoolIdLibrary, PoolId} from "v4-core/types/PoolId.sol";
-// import {Hooks} from "v4-core/libraries/Hooks.sol";
-// import {TickMath} from "v4-core/libraries/TickMath.sol";
-
-// // interfaces
-// import {IHooks} from "v4-core/interfaces/IHooks.sol";
-// import {IERC20Minimal} from "v4-core/interfaces/external/IERC20Minimal.sol";
-// import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
-
-// // pool manager
-// import {PoolManager} from "v4-core/PoolManager.sol";
-// // import {PoolModifyPositionTest} from "v4-core/test/PoolModifyPositionTest.sol";
-// import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
-
-// our contracts
+import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
+import {PoolId} from "v4-core/src/types/PoolId.sol";
+import {EasyPosm} from "./utils/EasyPosm.sol";
+import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {Fixtures} from "./utils/Fixtures.sol";
 import {LendingHook} from "../src/LendingHook.sol";
 import {LendingHookStub} from "./LendingHookStub.sol";
 
-contract LendingHookTest is Test, GasSnapshot {
-// use libs
+contract LendingHookTest is Test, GasSnapshot, Fixtures {
+    // use libs
+    using EasyPosm for IPositionManager;
+    using StateLibrary for IPoolManager;
+
+    LendingHook hook;
+    PoolId poolId;
+
+    uint256 tokenId;
+    int24 tickLower;
+    int24 tickUpper;
+
+    function setUp() public {
+        // creates the pool manager, utility routers, and test tokens
+        deployFreshManagerAndRouters();
+        deployMintAndApprove2Currencies();
+
+        deployAndApprovePosm(manager);
+
+        // Deploy the hook to an address with the correct flags
+        address flags = address(
+            uint160(Hooks.AFTER_SWAP_FLAG | Hooks.AFTER_ADD_LIQUIDITY_FLAG) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
+        );
+        bytes memory constructorArgs = abi.encode(manager); //Add all the necessary constructor arguments from the hook
+        deployCodeTo("LendingHook.sol:LendingHook", constructorArgs, flags);
+        hook = LendingHook(flags);
+        // pointsToken = hook.pointsToken();
+
+        // Create the pool
+        key = PoolKey(Currency.wrap(address(0)), currency1, 3000, 60, IHooks(hook));
+        poolId = key.toId();
+        manager.initialize(key, SQRT_PRICE_1_1);
+
+        // Provide full-range liquidity to the pool
+        tickLower = TickMath.minUsableTick(key.tickSpacing);
+        tickUpper = TickMath.maxUsableTick(key.tickSpacing);
+
+        deal(address(this), 200 ether);
+
+        (uint256 amount0, uint256 amount1) = LiquidityAmounts.getAmountsForLiquidity(
+            SQRT_PRICE_1_1,
+            TickMath.getSqrtPriceAtTick(tickLower),
+            TickMath.getSqrtPriceAtTick(tickUpper),
+            uint128(100e18)
+        );
+
+        (tokenId,) = posm.mint(
+            key,
+            tickLower,
+            tickUpper,
+            100e18,
+            amount0 + 1,
+            amount1 + 1,
+            address(this),
+            block.timestamp,
+            hook.getHookData(address(this))
+        );
+    }
 }
